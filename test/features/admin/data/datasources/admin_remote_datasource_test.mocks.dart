a29fbcd592af67356fb617c83cad9065c87efcce
tujii <inthusan@hotmail.de>
2025-12-16 00:42:36 +0100

    Remove unnecessary ignore directives for must_be_immutable in mock classes

--- test/features/admin/data/datasources/admin_remote_datasource_test.mocks.dart
+++ test/features/admin/data/datasources/admin_remote_datasource_test.mocks.dart
@@ -430,7 +430,6 @@ class MockFirebaseFirestore extends _i1.Mock implements _i4.FirebaseFirestore {
 /// A class which mocks [CollectionReference].
 ///
 /// See the documentation for Mockito's code generation for more information.
-// ignore: must_be_immutable
 class MockCollectionReference<T extends Object?> extends _i1.Mock
     implements _i4.CollectionReference<T> {
   MockCollectionReference() {
@@ -842,7 +841,6 @@ class MockCollectionReference<T extends Object?> extends _i1.Mock
 /// A class which mocks [DocumentReference].
 ///
 /// See the documentation for Mockito's code generation for more information.
-// ignore: must_be_immutable
 class MockDocumentReference<T extends Object?> extends _i1.Mock
     implements _i4.DocumentReference<T> {
   MockDocumentReference() {
@@ -1176,7 +1174,6 @@ class MockWriteBatch extends _i1.Mock implements _i4.WriteBatch {
 /// A class which mocks [Query].
 ///
 /// See the documentation for Mockito's code generation for more information.
-// ignore: must_be_immutable
 class MockQuery<T extends Object?> extends _i1.Mock implements _i4.Query<T> {
   MockQuery() {
     _i1.throwOnMissingStub(this);
